3f3ec86b81a872d844e830965d4fb5ba378b46c6
RadoKolev <rado.k.kolev@gmail.com>
2024-08-09 17:38:31 +0100

    Add type "never"

--- basics/src/basics.ts
+++ basics/src/basics.ts
@@ -1306,3 +1306,48 @@ try {
 
 
 // ! *** Type 'never' ***
+// ? "Never" is a type that represents the type of values that never occur.you can't assign any value to a variable of type never. TypeScript will give a compile error if there are any unhandled cases, helping ensure that all cases are handled.
+
+// let someValue: never = 0; //* A value can not be assigned to type "never"
+
+type Theme1 = 'light' | 'dark';
+
+function checkTheme(theme: Theme1) {
+  if (theme === 'light') {
+    console.log('light theme');
+    return;
+  }
+
+  if (theme === 'dark') {
+    console.log('dark theme');
+    return;
+  }
+
+  theme;
+  // theme is of type never, because it can never have a value that is not 'light' or 'dark'.
+}
+
+
+enum Color {
+  Red,
+  Blue,
+  // Green,
+}
+
+function getColorName(color: Color) {
+  switch (color) {
+    case Color.Red:
+      return 'Red';
+    case Color.Blue:
+      return 'Blue';
+    default:
+      // at build time
+      let unexpectedColor: never = color;
+      // at runtime
+      throw new Error(`Unexpected color value: ${unexpectedColor}`);
+  }
+}
+
+console.log(getColorName(Color.Red)); // Red
+console.log(getColorName(Color.Blue)); // Blue
+// console.log(getColorName(Color.Green)); // Green
