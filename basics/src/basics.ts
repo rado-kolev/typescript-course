8ff7107f1eeb00025ed2949dfb1f7ed640ad4847
RadoKolev <rado.k.kolev@gmail.com>
2024-08-10 22:02:43 +0100

    Add fetch data

--- basics/src/basics.ts
+++ basics/src/basics.ts
@@ -4,6 +4,8 @@
 // TODO ----> For contrast comments is used the VS Code extension Better Comments
 // *****************************************************************************
 
+import { z } from 'zod';
+
 
 // ! *** Type Annotations ***
 // ? --> Allow developers to specify the types of variables, function parameters, return types, and object properties.
@@ -1891,3 +1893,134 @@ const randomStuff: StoreData = {
   data: ['random', 1],
 };
 
+
+
+
+// ! *** Fetch Data ***
+
+//* Won't set any state values
+
+// const url = 'https://www.course-api.com/react-tours-project';
+
+// async function fetchData(url: string) {
+//   try {
+//     const response = await fetch(url);
+
+     //* Check if the request was successful.
+//     if (!response.ok) {
+//       throw new Error(`HTTP error! status: ${response.status}`);
+//     }
+
+//     const data = await response.json();
+//     return data;
+//   } catch (error) {
+//     const errMsg =
+//       error instanceof Error ? error.message : 'there was an error...';
+//     console.error(errMsg);
+     //* throw error;
+//     return [];
+//   }
+// }
+
+// const tours = await fetchData(url);
+// tours.map((tour: any) => {
+//   console.log(tour.name);
+// });
+
+//* return empty array
+//* throw error in catch block
+//* we are not setting state values in this function
+
+
+// TODO *** Setup type and provide correct return type for the above example ***
+
+// const url = 'https://www.scourse-api.com/react-tours-project';
+
+//* Define a type for the data you're fetching.
+
+// type Tour = {
+//   id: string;
+//   name: string;
+//   info: string;
+//   image: string;
+//   price: string;
+  //* Add more fields as necessary.
+// };
+
+// async function fetchData(url: string): Promise<Tour[]> {
+//   try {
+//     const response = await fetch(url);
+
+    //* Check if the request was successful.
+  //   if (!response.ok) {
+  //     throw new Error(`HTTP error! status: ${response.status}`);
+  //   }
+
+  //   const data: Tour[] = await response.json();
+  //   console.log(data);
+  //   return data;
+  // } catch (error) {
+  //   const errMsg =
+  //     error instanceof Error ? error.message : 'there was an error...';
+  //   console.error(errMsg);
+
+    //* throw error;
+//     return [];
+//   }
+// }
+
+// const tours = await fetchData(url);
+// tours.map((tour) => {
+//   console.log(tour.name);
+// });
+
+
+// TODO *** Fetch Data using Zod ***
+
+//* First - npm install zod
+
+// import { z } from 'zod';
+
+const url = 'https://www.course-api.com/react-tours-project';
+
+const tourSchema = z.object({
+  id: z.string(),
+  name: z.string(),
+  info: z.string(),
+  image: z.string(),
+  price: z.string(),
+  // something: z.string(),
+});
+
+// extract the inferred type
+type Tour = z.infer<typeof tourSchema>;
+
+async function fetchData(url: string): Promise<Tour[]> {
+  try {
+    const response = await fetch(url);
+
+    // Check if the request was successful.
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const rawData: Tour[] = await response.json();
+    const result = tourSchema.array().safeParse(rawData);
+    if (!result.success) {
+      throw new Error(`Invalid data: ${result.error}`);
+    }
+    return result.data;
+  } catch (error) {
+    const errMsg =
+      error instanceof Error ? error.message : 'there was an error...';
+    console.log(errMsg);
+
+    // throw error;
+    return [];
+  }
+}
+
+const tours = await fetchData(url);
+tours.map((tour) => {
+  console.log(tour.name);
+});
